a7efff63d462c47d2038a714a0d3ba34e441f17f
Corentin Lumineau <corentin.lumineau@e-xpertsolutions.com>
2024-09-04 15:33:04 +0200

    feat: update message landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-300">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white mb-4">Ma Chérie Adorée</h1>
+        <p className="text-xl text-white">
+          Mon amour pour toi est aussi infini que l&apos;océan, aussi brillant que les étoiles. Chaque jour à tes côtés est un cadeau précieux. Tu es ma joie, mon inspiration, et mon plus beau rêve devenu réalité. Je t&apos;aime plus que les mots ne peuvent l&apos;exprimer.
+        </p>
+      </div>
+    </div>
+  );
 }
